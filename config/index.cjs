e3fb79dbd0319e48950c376a020f9f03c2bbdd5f
wenhanbaidai <46233890+wenhanbaidai@users.noreply.github.com>
2023-05-14 19:09:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0vdw6QJy6_fqh2xR3IPz9y8Pcvw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rw2kTPryGaoOIiVo22c9WW1P6j-UYrr4HjnF1mEpSZE',
+      useTemplateId: 'xMcbt6JLET1-B7hA-TCFEwGf4o6-u72CgIUtxJQIk-s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-03',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
