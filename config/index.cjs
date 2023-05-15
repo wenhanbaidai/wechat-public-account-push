e392e796b5d5ed7da44361c57b927dde636c27e0
wenhanbaidai <46233890+wenhanbaidai@users.noreply.github.com>
2023-05-15 08:56:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -94,7 +94,7 @@ const USER_CONFIG = {
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: true,
+    morningGreeting: false,
   
     // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     eveningGreeting: false,
